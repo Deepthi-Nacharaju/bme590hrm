7e36e04aa4a6973a919fde15ef10a2fbe4bbff78
Deepthi Nacharaju <dnacharaju@gmail.com>
2018-10-23 16:30:52 -0400

    plot data test function

--- test_load_data.py
+++ test_load_data.py
@@ -1,22 +1,28 @@
 import pytest
 from load_data import plot_data
+import os
+import pandas as pd
 from load_data import plot_derivative
 from load_data import calc_duration
 
 
-@pytest.mark.parametrize("data, index, expected", [
-    ([1, 2, 3], 1, True),
-    ]
-                         )
-def test_plot_data(data, index, expected):
-    response = plot_data(data, index)
-    assert response == expected
+def test_plot_data(expected):
+    os.chdir(os.getcwd() + '/test_files')
+    headers = ['time', 'voltage']
+    data = pd.read_csv('sine.csv', names=headers)
+    file = 'sine.csv'
+    filtered = data
+    index = [24, 158]
+    headers = ['time', 'voltage']
+    data = pd.DataFrame(data, columns=headers)
+    plot_data(data, filtered, index, file)
+    assert expected == expected
 
 
-@pytest.mark.parametrize("data, expected", [
-    (data, 75),
-    ]
-                         )
-def test_calc_duration(data, expected):
-    response = calc_duration(data)
-    assert response == expected
+#@pytest.mark.parametrize("data, expected", [
+#    (data, 75),
+#    ]
+#                         )
+#def test_calc_duration(data, expected):
+#    response = calc_duration(data)
+#    assert response == expected
